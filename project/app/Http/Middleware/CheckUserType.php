49980a95df33b264a8c533408d5fa1ed9f669db1
guilhermematoso@letsgrow.com.br <guimatoso15@gamil.com>
2020-08-30 02:02:20 -0300

    Adicionando middleware de autenticação baseado no nivel de usuários

---
+++ project/app/Http/Middleware/CheckUserType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class CheckUserType
+{
+	public function handle($request, Closure $next, $userType)
+	{
+		if ($request->user()->hasRole($userType)) {
+			return $next($request);
+		}
+
+		if (in_production()) {
+			if ($request->wantsJson()) {
+				return response()->json([
+					'error' => 'Unauthorized.'
+				], 403);
+			}
+
+			flash()->error('Acesso não autorizado');
+			return redirect()->route('home');
+		}
+
+		$message = 'Forbidden Route. ';
+		$message .= "This resource was meant for '" . $this->getUserTypeLabel($userType) . "' and ";
+
+		return abort(403, $message);
+	}
+
+	private function getUserTypeLabel($userType)
+	{
+		return $userType;
+	}
+}
